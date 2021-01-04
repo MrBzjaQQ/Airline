730744c2ad639d01e00af7b047860f22e7fbb509
Ilya Ershov <ershov.1ly4@gmail.com>
2021-01-04 22:20:49 +0600

    AirlineSendAgent Implemented

--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using AirlineSendAgent.App;
+using AirlineSendAgent.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AirlineSendAgent.Client;
+using System.IO;
 
 namespace AirlineSendAgent
 {
@@ -6,7 +14,29 @@ namespace AirlineSendAgent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets(typeof(Program).Assembly, optional:true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+
+            var config = builder.Build();
+            
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>{
+                  services.AddSingleton<IAppHost, AppHost>();
+                  services.AddSingleton<IWebhookClient, WebhookClient>();
+                  services.AddDbContext<SendAgentDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("AirlineConnection")));
+                  //(config.GetConnectionString("AirlineConnection")));
+                                                                
+                  
+                    
+                  services.AddHttpClient();
+                }).Build();
+
+            host.Services.GetService<IAppHost>().Run();
         }
     }
 }
